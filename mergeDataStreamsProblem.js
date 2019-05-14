7bddabfdd5111bcd2131e235a09d2a4c0e9ed75e
Kristen Doucette <cosmicnoir90@gmail.com>
2019-05-13 20:48:06 -0600

    Solution 1.0

--- mergeDataStreamsProblem.js
+++ mergeDataStreamsProblem.js
@@ -40,16 +40,21 @@ function mergeDataStreams(arr1, arr2) {
     for (let i = 0; i < arr1.length; i++) {
         for (let j = 0; j < arr2.length; j++) {
             if (arr1[i].id === arr2[j].id) {
-                for (let key in arr2[j]) {
-                    let exists = arr1[i][key];
-                    if (exists === undefined) {
-                        arr1[i][key] = arr2[j]; // Should create the key/value pair of arr2[j][key] value and add to the object at index i of arr1, with key [key]. 
+              console.log("Should log if ids match"); // Saying only happening once
+                for (let key in arr2[j]) { // for each key on the object at index j of array2,
+                  console.log(key); // Logs id, occupation, address! 
+                    let exists = arr1[i][key]; // Prints value of 
+                    console.log(exists);
+                    if (exists === undefined) { // know console is pringing undefined
+                    arr1[i][key] = arr2[j][key];
+                        
                     }
                 }
-            return arr1; // Should return the array with new key/value pairs. 
+            
             }
         }
     }
+    return arr1; // Should return the array with new key/value pairs. 
 }
 
 let resultedArray = mergeDataStreams(arrayOne, arrayTwo);
